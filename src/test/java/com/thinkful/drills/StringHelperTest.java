934e94e71ff4bb4b67f035adbcee2bac5d903d4c
Vincent Ramdhanie <vramdhanie@gmail.com>
2018-04-29 22:56:09 -0400

    Test for isNumeric and contains

--- src/test/java/com/thinkful/drills/StringHelperTest.java
+++ src/test/java/com/thinkful/drills/StringHelperTest.java
@@ -12,9 +12,25 @@ public class StringHelperTest {
   public void isNumericTest() {
     StringHelper helper = new StringHelper("20");
     StringHelper helper2 = new StringHelper("abc");
+    StringHelper helper3 = new StringHelper("3.14");
+    StringHelper helper4 = new StringHelper("-1");
 
-    assertTrue("20 should be numeric", helper.isNumeric());
+    assertTrue("Integer - 20 should be numeric", helper.isNumeric());
     assertFalse("abc should not be numeric", helper2.isNumeric());
+    assertTrue("Real - 3.14 should be numeric", helper3.isNumeric());
+    assertTrue("Negative - -1 should be numeric", helper4.isNumeric());
+  }
+
+  @Test
+  public void containsTest() {
+    StringHelper helper = new StringHelper("Here is a test String");
+    assertTrue("should contain \"test\"", helper.contains("test"));
+    assertTrue("should contain \"Here\"", helper.contains("Here"));
+    assertTrue("should contain \"String\"", helper.contains("String"));
+    assertTrue("should contain \"re a\"", helper.contains("re a"));
+    assertTrue("should contain blank", helper.contains(""));
+    assertFalse("should not contain \"there\"", helper.contains("there"));
+    assertFalse("should not contain \"Hereis\"", helper.contains("Hereis"));
   }
 
 
